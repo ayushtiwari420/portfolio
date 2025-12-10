53f177ee182b5839d7e8eed0987c7290d8a5de06
ayushtiwari420 <161028129+ayushtiwari420@users.noreply.github.com>
2025-12-10 14:49:58 +0530

    Update emailjs import to use @emailjs/browser

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
-import emailjs from "emailjs-com";
+import emailjs from "@emailjs/browser";
 import { motion } from "framer-motion";
 
 
